88282026247bd742635116a32afe9b7166c388f5
unknown <dtaylor@OE1756.uk.ioko365.com>
2013-03-20 17:03:26 -0400

    adding some test files.

---
+++ php/test.php
@@ -0,0 +1,52 @@
+<?php
+$getvars = array('traffic','site','direction');
+
+
+foreach($getvars as $var)
+{
+  if (isset($_GET[$var])) {
+    $$var = $_GET[$var];
+  }
+}
+
+// sanity
+if(!isset('site') || !isset('traffic') || !isset('direction')) return;
+
+
+$V1 ="";
+$TS2 = new Date().getTime();
+$TS1 = $TS2-3600*10;
+
+/* If "site" is all, then do nothing.. Otherwise V1="site=XXX"
+*/
+if ( $site != "ALL")
+{
+  $V1="site=".$site;
+}
+/* If "traffic type" is both, then do nothing.. Otherwise V1 += ",type=YYY" (or if V1="", ignore the ","..) 
+*/
+if ( $traffic != "BOTH")
+{
+  if ( $site != "ALL")
+  { 
+    $V1 = $V1 . ",type=" .$site;
+  }
+}
+/* If direction is "inbound", V2="redlining.bytes_received", else V2="redlining.bytes_sent"
+*/
+if ( $direction == "IN" )
+{
+  $V2 = "redlining.bytes_received";
+}
+else
+{
+  $V2 ="redlining.bytes_sent";
+}
+
+if ( $V1 != "" )
+{
+  $V1 = "{".$V1."}";
+}
+
+echo php_curl("http://10.1.0.67:4242/q?start=".$TS1."&end=".$TS2."&m=sum:".$V2.$V1."&ascii");
+
